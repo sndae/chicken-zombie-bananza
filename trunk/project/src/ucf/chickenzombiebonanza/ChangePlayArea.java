16a06f35b8c324fe09d0fab72a529832b94d33ca
cronox@gmail.com <cronox@gmail.com@918e7a11-6bd4-af16-1358-e36828d21c24>
2011-11-30 02:39:07 +0000

    Forgot to actually add play area.

---
+++ trunk/project/src/ucf/chickenzombiebonanza/ChangePlayArea.java
@@ -0,0 +1,21 @@
+package ucf.chickenzombiebonanza;
+
+import ucf.chickenzombiebonanza.game.GameStateEnum;
+import ucf.chickenzombiebonanza.game.GameStateListener;
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+
+public class ChangePlayArea extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+	    
+
+}
